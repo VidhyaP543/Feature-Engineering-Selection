d4376e3364835e9cd8a349c4eb27062655e4fa96
VidhyaP543 <apoorvaapoorva446@gmail.com>
2026-06-27 12:14:05 +0530

    Update Feature_Engineering.py

--- scripts/Feature_Engineering.py
+++ scripts/Feature_Engineering.py
@@ -1,7 +1,7 @@
 import pandas as pd
 from sklearn.preprocessing import LabelEncoder, StandardScaler
 from sklearn.feature_selection import SelectKBest, chi2
-df = pd.read_csv("C:/Users/Sharath/Desktop/DataScience Intern/Feature-Engineering-Selection/data/train.csv")
+df = pd.read_csv("data/train.csv")
 df["Age"] = df["Age"].fillna(df["Age"].median())
 df["Embarked"] = df["Embarked"].fillna(df["Embarked"].mode()[0])
 df.drop(columns=["Cabin"], inplace=True)
@@ -19,5 +19,5 @@ print("Selected Features:")
 print(selected_features)
 engineered = pd.DataFrame(X_new, columns=selected_features)
 engineered["Survived"] = y
-engineered.to_csv("C:/Users/Sharath/Desktop/DataScience Intern/Feature-Engineering-Selection/output/engineered_features.csv", index=False)
+engineered.to_csv("output/engineered_features.csv", index=False)
 print("engineered_features.csv created successfully!")
